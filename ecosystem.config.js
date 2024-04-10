62ee99a2e30909a0f0eef93949a42cd2d4ec1437
lluz <luisluz@gmail.com>
2024-04-10 01:50:52 +0100

    fix ?

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "app-name",
+      name: "udemy-next-2",
       script: "./node_modules/.bin/next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
